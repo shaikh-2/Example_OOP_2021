3845ac46801600b3b96219b5e170f5d162611d00
shaikh <ahmed.shaikh@hsrw.org>
2021-10-07 13:40:15 +0200

    try 2

---
+++ QuickStart.java
@@ -0,0 +1,11 @@
+public class QuickStart {
+    public class HelloWorld{
+
+        public static void main(String[] args) {
+           int a=10;
+           int b=20;
+           int c=a+b;
+            System.out.println("Hello World 2021 :"+c);
+        }
+    }
+}
